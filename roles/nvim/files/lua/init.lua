974380e29069c59cb84866d114b13b82b20da2c1
lpalma <linus.palma@gmail.com>
2026-07-05 09:45:19 +0200

    feat(nvim): add stylua.toml for reduced line breaking in auto formatter. change telescope for ff and fg to snacks picker

--- roles/nvim/files/lua/init.lua
+++
@@ -1,6 +0,0 @@
-require("config.vim")
-require("config.lazy")
-
--- wenn nach branch wechsel Lazy error in der GUI für die checks anzeigt dann die packages einmal löschen:
--- rm -rf ~/.local/share/nvim/lazy/mason-lspconfig.nvim
--- rm -rf ~/.local/share/nvim/lazy/mason.nvim
--- roles/nvim/files/lua/plugins/snacks.lua
+++ roles/nvim/files/lua/plugins/snacks.lua
@@ -1,31 +1,30 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  keys = {
-    { "<leader>gg", function() Snacks.lazygit() end,          desc = "Lazygit" },
-    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
-    { "<leader>gl", function() Snacks.lazygit.log() end,      desc = "Lazygit Log (cwd)" },
-  },
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    animate = { enable = true },
-    bigfile = { enabled = true },
-    dashboard = { enabled = true },
-    explorer = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
-    image = { enabled = true },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    lazygit = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	keys = {
+		{ "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+		{ "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep (live)" },
+		{ "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+		{ "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+		{ "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+	},
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		animate = { enable = true },
+		bigfile = { enabled = true },
+		dashboard = { enabled = true },
+		explorer = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		image = { enabled = true },
+		picker = { enabled = true },
+		notifier = { enabled = true },
+		lazygit = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = true },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+	},
 }
